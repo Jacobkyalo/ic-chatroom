bcf620e6a11950f7937b8f8ec41d8d14df5a5162
Jacob Kyalo <jacobkyalo43@gmail.com>
2024-02-20 12:42:44 +0300

    added close func to each room

--- src/components/room-item.jsx
+++ src/components/room-item.jsx
@@ -1,9 +1,9 @@
 import { PropTypes } from "prop-types";
 import { Link } from "react-router-dom";
 
-export default function RoomItem({ room }) {
+export default function RoomItem({ room, closeCommunities }) {
   return (
-    <li className="w-full">
+    <li className="w-full" onClick={closeCommunities}>
       <Link
         to={`${room?.$id}`}
         className="flex items-center gap-x-2 rounded px-2 py-3 text-neutral-700 transition-all duration-150 ease-in-out hover:bg-ic-green hover:bg-opacity-30 hover:text-neutral-900"
@@ -21,4 +21,5 @@ RoomItem.propTypes = {
     href: PropTypes.string.isRequired,
     $id: PropTypes.string.isRequired,
   }).isRequired,
+  closeCommunities: PropTypes.func.isRequired,
 };
