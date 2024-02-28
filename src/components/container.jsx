ed159d6f096ecfc91e8d326ae8445a34728e0f66
Jacob Kyalo <jacobkyalo43@gmail.com>
2024-02-28 10:47:04 +0300

    updated container component

--- src/components/container.jsx
+++ src/components/container.jsx
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
 
 export default function Container({ children }) {
-  return <div className="container mx-auto w-full">{children}</div>;
+  return <div className="container mx-auto w-full max-w-3xl">{children}</div>;
 }
 
 Container.propTypes = {
