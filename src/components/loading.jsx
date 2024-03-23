e6ba932b2b5e4c53863561748d75a1d2b28d618a
Jacob Kyalo <jacobkyalo43@gmail.com>
2024-03-23 20:40:08 +0300

    add loading component

---
+++ src/components/loading.jsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+  return (
+    <main>
+      <section className="flex h-screen w-full items-center justify-center">
+        <div className="h-14 w-14 animate-spin rounded-full border-4 border-ic-green border-t-transparent" />
+      </section>
+    </main>
+  );
+}
