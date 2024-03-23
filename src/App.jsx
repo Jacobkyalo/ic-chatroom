3351b79b14908360221c907bf10f7fbd125528b0
Jacob Kyalo <jacobkyalo43@gmail.com>
2024-03-23 20:41:38 +0300

    render outlet and loading components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
-import { Outlet } from "react-router-dom";
+import { Suspense, lazy } from "react";
 import AuthContextProvider from "./contexts/auth-context";
 import { ToastContainer } from "react-toastify";
+const OutletComponent = lazy(() => import("./components/outlet"));
+import Loading from "./components/loading";
 import "react-toastify/dist/ReactToastify.css";
 
 export default function App() {
@@ -8,7 +10,9 @@ export default function App() {
     <AuthContextProvider>
       <div className="relative px-4 md:px-0">
         <ToastContainer />
-        <Outlet />
+        <Suspense fallback={<Loading />}>
+          <OutletComponent />
+        </Suspense>
       </div>
     </AuthContextProvider>
   );
