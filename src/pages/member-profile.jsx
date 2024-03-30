d4ccfaf8b9881d7915c5427c4801605a3daea341
Jacob Kyalo <jacobkyalo43@gmail.com>
2024-03-30 10:53:50 +0300

    add react-helmet for member-profile page

--- src/pages/member-profile.jsx
+++ src/pages/member-profile.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Helmet } from "react-helmet";
 import axios from "axios";
 import { Link, useParams } from "react-router-dom";
 import { toast } from "react-toastify";
@@ -30,39 +31,54 @@ export default function MemberProfile() {
   }, [params.id]);
 
   return (
-    <main>
-      <section className="mx-auto mt-16 flex h-auto w-full max-w-xl flex-col items-center justify-center rounded-lg bg-white p-4 text-center shadow-2xl sm:p-10">
-        {loading ? (
-          "Loading..."
-        ) : (
-          <>
-            <Link className="mb-6 text-start hover:text-ic-green" to="/">
-              &larr; Back
-            </Link>
-            <div className="w-full">
-              <div className="mx-auto mb-6 flex h-24 w-24 items-center justify-center rounded-full bg-ic-green text-white">
-                {/* <span className="text-2xl">{sliceName(member?.name)}</span> */}
-                <VscAccount size={40} />
-              </div>
-              <div className="mb-6">
-                <span className="text-xl text-neutral-600">{member?.name}</span>
-              </div>
+    <>
+      <Helmet>
+        <title>Member Profile | Chatroom - Innovation Club</title>
+        <meta
+          name="description"
+          content="Member profile for Innovation Club Chatroom"
+        />
+        <meta
+          name="keywords"
+          content="Chatroom system, Meru University of Science and Technology, Meru University Innovation Club, Meru University Innovation Club Chatroom, Meru University Innovation Club Chatroom System, Innovation Club, Innovation Club Chatroom, Innovation Club Chatroom System"
+        />
+      </Helmet>
+      <main>
+        <section className="mx-auto mt-16 flex h-auto w-full max-w-xl flex-col items-center justify-center rounded-lg bg-white p-4 text-center shadow-2xl sm:p-10">
+          {loading ? (
+            "Loading..."
+          ) : (
+            <>
+              <Link className="mb-6 text-start hover:text-ic-green" to="/">
+                &larr; Back
+              </Link>
+              <div className="w-full">
+                <div className="mx-auto mb-6 flex h-24 w-24 items-center justify-center rounded-full bg-ic-green text-white">
+                  {/* <span className="text-2xl">{sliceName(member?.name)}</span> */}
+                  <VscAccount size={40} />
+                </div>
+                <div className="mb-6">
+                  <span className="text-xl text-neutral-600">
+                    {member?.name}
+                  </span>
+                </div>
 
-              <div className="flex w-full flex-col gap-y-4">
-                <MemberDetailsCard member={member} />
-              </div>
+                <div className="flex w-full flex-col gap-y-4">
+                  <MemberDetailsCard member={member} />
+                </div>
 
-              <button
-                type="button"
-                onClick={() => alert("Not implemented yet!")}
-                className="mt-10 rounded bg-ic-green px-6 py-3 text-white"
-              >
-                Message
-              </button>
-            </div>
-          </>
-        )}
-      </section>
-    </main>
+                <button
+                  type="button"
+                  onClick={() => alert("Not implemented yet!")}
+                  className="mt-10 rounded bg-ic-green px-6 py-3 text-white"
+                >
+                  Message
+                </button>
+              </div>
+            </>
+          )}
+        </section>
+      </main>{" "}
+    </>
   );
 }
