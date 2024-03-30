0a2a3c5837db830502492c6510156f95b8f72602
Jacob Kyalo <jacobkyalo43@gmail.com>
2024-03-30 10:52:35 +0300

    add react-helmet for reset password page

--- src/pages/reset-password.jsx
+++ src/pages/reset-password.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Helmet } from "react-helmet";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
@@ -44,87 +45,100 @@ export default function ResetPassword() {
   };
 
   return (
-    <main>
-      <section className="flex items-center justify-center lg:h-screen lg:items-start">
-        <div className="hidden h-full flex-1 self-center bg-ic-green bg-opacity-30 lg:block">
-          <img
-            src={ICIllustration}
-            alt="ic_illustration"
-            className="mt-10 h-3/4 w-full object-contain"
-          />
-        </div>
-        <div className="w-full py-10 md:max-w-xl md:px-10 md:py-10">
-          <div className="mb-8">
-            <LogoHeader />
+    <>
+      <Helmet>
+        <title>Reset Password | Chatroom - Innovation Club</title>
+        <meta
+          name="description"
+          content="Reset password to continue to Innovation Club Chatroom"
+        />
+        <meta
+          name="keywords"
+          content="Chatroom system, Meru University of Science and Technology, Meru University Innovation Club, Meru University Innovation Club Chatroom, Meru University Innovation Club Chatroom System, Innovation Club, Innovation Club Chatroom, Innovation Club Chatroom System"
+        />
+      </Helmet>
+      <main>
+        <section className="flex items-center justify-center lg:h-screen lg:items-start">
+          <div className="hidden h-full flex-1 self-center bg-ic-green bg-opacity-30 lg:block">
+            <img
+              src={ICIllustration}
+              alt="ic_illustration"
+              className="mt-10 h-3/4 w-full object-contain"
+            />
           </div>
-          <div className="mb-6">
-            <h2 className="mb-1 text-2xl font-bold text-black">
-              Reset Your Password
-            </h2>
-            <p className="text-sm text-neutral-600">
-              Enter your new password here.
-            </p>
-          </div>
-
-          <form onSubmit={handleSubmit(onSubmit)} className="block">
-            <div className="mb-4">
-              <label htmlFor="password" className="relative block">
-                <span className="mb-1 block">New Password</span>
-                <input
-                  {...register("password")}
-                  type={showPassword ? "text" : "password"}
-                  placeholder="New Password"
-                  name="password"
-                  id="password"
-                  autoComplete="off"
-                  className="w-full rounded border border-neutral-600 p-3 outline-none placeholder:text-sm focus:border-ic-green focus:ring-1 focus:ring-ic-green"
-                />
-                <span
-                  role="button"
-                  onClick={() => setShowPassword(!showPassword)}
-                  className="absolute right-3 top-1/2 translate-y-1/2 transform text-lg"
-                >
-                  {showPassword ? <FaEyeSlash /> : <FaEye />}
-                </span>
-                <span className="mt-1 block text-sm text-red-600 first-letter:uppercase">
-                  {errors.password?.message}
-                </span>
-              </label>
+          <div className="w-full py-10 md:max-w-xl md:px-10 md:py-10">
+            <div className="mb-8">
+              <LogoHeader />
             </div>
-            <div className="mb-4">
-              <label htmlFor="passwordAgain" className="relative block">
-                <span className="mb-1 block">Confirm New Password</span>
-                <input
-                  {...register("passwordAgain")}
-                  type={showPasswordAgain ? "text" : "password"}
-                  placeholder="Confirm New Password"
-                  name="passwordAgain"
-                  id="passwordAgain"
-                  autoComplete="off"
-                  className="w-full rounded border border-neutral-600 p-3 outline-none placeholder:text-sm focus:border-ic-green focus:ring-1 focus:ring-ic-green"
-                />
-                <span
-                  role="button"
-                  onClick={() => setShowPasswordAgain(!showPasswordAgain)}
-                  className="absolute right-3 top-1/2 translate-y-1/2 transform text-lg"
-                >
-                  {showPasswordAgain ? <FaEyeSlash /> : <FaEye />}
-                </span>
-                <span className="mt-1 block text-sm text-red-600 first-letter:uppercase">
-                  {errors.passwordAgain?.message}
-                </span>
-              </label>
+            <div className="mb-6">
+              <h2 className="mb-1 text-2xl font-bold text-black">
+                Reset Your Password
+              </h2>
+              <p className="text-sm text-neutral-600">
+                Enter your new password here.
+              </p>
             </div>
 
-            <button
-              type="submit"
-              className="my-4 w-full rounded bg-ic-green py-3 text-base text-white"
-            >
-              {loading ? "Please wait..." : "Reset Password"}
-            </button>
-          </form>
-        </div>
-      </section>
-    </main>
+            <form onSubmit={handleSubmit(onSubmit)} className="block">
+              <div className="mb-4">
+                <label htmlFor="password" className="relative block">
+                  <span className="mb-1 block">New Password</span>
+                  <input
+                    {...register("password")}
+                    type={showPassword ? "text" : "password"}
+                    placeholder="New Password"
+                    name="password"
+                    id="password"
+                    autoComplete="off"
+                    className="w-full rounded border border-neutral-600 p-3 outline-none placeholder:text-sm focus:border-ic-green focus:ring-1 focus:ring-ic-green"
+                  />
+                  <span
+                    role="button"
+                    onClick={() => setShowPassword(!showPassword)}
+                    className="absolute right-3 top-1/2 translate-y-1/2 transform text-lg"
+                  >
+                    {showPassword ? <FaEyeSlash /> : <FaEye />}
+                  </span>
+                  <span className="mt-1 block text-sm text-red-600 first-letter:uppercase">
+                    {errors.password?.message}
+                  </span>
+                </label>
+              </div>
+              <div className="mb-4">
+                <label htmlFor="passwordAgain" className="relative block">
+                  <span className="mb-1 block">Confirm New Password</span>
+                  <input
+                    {...register("passwordAgain")}
+                    type={showPasswordAgain ? "text" : "password"}
+                    placeholder="Confirm New Password"
+                    name="passwordAgain"
+                    id="passwordAgain"
+                    autoComplete="off"
+                    className="w-full rounded border border-neutral-600 p-3 outline-none placeholder:text-sm focus:border-ic-green focus:ring-1 focus:ring-ic-green"
+                  />
+                  <span
+                    role="button"
+                    onClick={() => setShowPasswordAgain(!showPasswordAgain)}
+                    className="absolute right-3 top-1/2 translate-y-1/2 transform text-lg"
+                  >
+                    {showPasswordAgain ? <FaEyeSlash /> : <FaEye />}
+                  </span>
+                  <span className="mt-1 block text-sm text-red-600 first-letter:uppercase">
+                    {errors.passwordAgain?.message}
+                  </span>
+                </label>
+              </div>
+
+              <button
+                type="submit"
+                className="my-4 w-full rounded bg-ic-green py-3 text-base text-white"
+              >
+                {loading ? "Please wait..." : "Reset Password"}
+              </button>
+            </form>
+          </div>
+        </section>
+      </main>
+    </>
   );
 }
